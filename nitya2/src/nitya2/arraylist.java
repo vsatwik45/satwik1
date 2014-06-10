7ff9c5d8d466ae4213843ee84982bbc65692f52b
vsatwik45 <vsatwik45@gmail.com>
2014-06-10 11:16:10 -0700

    second commit

---
+++ nitya2/src/nitya2/arraylist.java
@@ -0,0 +1,22 @@
+package nitya2;
+import java.util.ArrayList;
+public class arraylist {
+	
+
+	
+
+	  public static void main ( String[] args)
+	  {
+	    ArrayList<String> names = new ArrayList<String>();
+	    names.add("satwik");
+	    names.add("vamshi");
+	    names.add("pavan");
+	       
+	    
+	    System.out.println("employee1: " + names.get(0) );
+	    System.out.println("employee2: " + names.get(1) );
+	    System.out.println("employee3: " + names.get(2) );
+	  }
+	}
+
+
